5c56c4f5495393e1546abb85a6d4ef1161d5243f
Zach Tirrell <zbtirrell@gmail.com>
2013-08-28 08:42:30 -0400

    updating comment header block

--- go-git.php
+++ go-git.php
@@ -1,19 +1,18 @@
 <?php
-/*
-Plugin Name: GigaOM Git
-Version: 0.1
-Plugin URI: http://gigaom.com
-Description: Handy-dandy plugin to expose git information to a WordPress install
-Author: Matthew Batchelder
-Author URI: http://gigaom.com
-Tags: git
-Tested up to: 3.4.2
-License: GPL2
-*/
+/**
+ * Plugin Name: Gigaom Git
+ * Version: 0.1
+ * Plugin URI: http://gigaom.com
+ * Description: Handy-dandy plugin to expose git information to a WordPress install
+ * Author: Matthew Batchelder
+ * Author URI: http://gigaom.com
+ * Tags: git
+ * Tested up to: 3.4.2
+ * License: GPL2
+ */
 
 if ( is_admin() )
 {
-
 	include_once __DIR__ . '/components/class-go-git.php';
 	include_once __DIR__ . '/components/functions.php';
 
